ab952f79715c6a1acfeb823db3a8c20961c9e310
Leonardo <cruzleonardo831@gmail.com>
2022-10-24 14:35:48 -0500

    broken code

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-// app.use(morgan('dev'));
 const express = require("express");
+const morgan=require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      </head>
+      <body>
+        <ul>
+          ${posts.map(post => `<li>${/* post data*/}</li>`)}
+        </ul>
+      </body>
+      </html>`;
+
+      res.send(html)
+});
 
 const PORT = 1337;
 
