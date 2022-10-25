628b97e7fa9f6f3c4c635e762812084c6a4a71c6
Robby Bacus <robbybacus@gmail.com>
2022-10-25 11:45:55 -0400

    working home, post, error handling

--- app.js
+++ app.js
@@ -1,28 +1,70 @@
 const express = require("express");
-const morgan=require("morgan");
+const morgan = require("morgan");
 const postBank = require("./postBank");
 
 const app = express();
 
+app.use(morgan("dev"));
 
-app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-
   const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span><a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+
+  if (!post.id) {
+    // If the post wasn't found, just throw an error
+    throw new Error('Not Found')
+  }
+  // ... Otherwise, send the regular post detail HTML
+
+  res.send(`<!DOCTYPE html>
   <html>
     <head>
       <title>Wizard News</title>
-      </head>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
       <body>
-        <ul>
-          ${posts.map(post => `<li>${/* post data*/}</li>`)}
-        </ul>
+        <div class="news-list">
+          <header><img src="/logo.png"/>Wizard News</header>
+          <p><span>${post.title}</span>
+          <small>(by ${post.name})</small></p>
+          <div>${post.content}</div>
+        </div>
       </body>
-      </html>`;
-
-      res.send(html)
+      </html>`);
 });
 
 const PORT = 1337;
@@ -30,4 +72,6 @@ const PORT = 1337;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+
 
